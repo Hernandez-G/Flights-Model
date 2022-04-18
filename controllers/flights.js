985401577422aaa976bd399ba62dbe5745474372
hernandez-g <g5hernandez@yahoo.com>
2022-04-18 15:18:48 -0700

    an update

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,30 +1,29 @@
-const { render } = require("ejs");
-const flight = require("../models/flight");
 const Flight = require("../models/flight");
 
 module.exports = {
+    index,
     new: newFlight,
-    create,
-    index
+    create
 };
 
+
 function index(req, res) {
-    flight.find({}, function(err, flights){
-        res.render('flights/index', { flights });
+    Flight.find({}, function(err, flights){
+        res.render('flights/index', { title: 'All Flights', flights });
     });
 }
 
+function newFlight(req, res) {
+res.render('flights/new');
+}
+
+
 function create(req, res){
-    req.body.departDate = req.body.departDate;
-    if (req.body.departDate) req.body.departDate = req.body.departDate.split(/\s*,\s*/);
     const flight = new Flight(req.body);
+    console.log(req.body);
     flight.save(function(err){
         if (err) return res.render('flights/new');
      res.redirect('/flights')
     });
- }
- 
-
-function newFlight(req, res) {
-    res.render('flights/new');
 }
+
--- routes/flights.js
+++ routes/flights.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const flightsCtrl = require('../controllers/flights');
 
-router.get('/new', flightsCtrl.new);
 router.get('/', flightsCtrl.index); 
+router.get('/new', flightsCtrl.new);
 
 router.post('/', flightsCtrl.create);
 
 module.exports = router;
+
